009ea7d9f03530c06c48c5ea3a6e39616475727e
dii-gineer <diana.mendoza.rmz@gmail.com>
2018-12-11 14:26:26 -0600

    Updates

---
+++ precache-manifest.fe749c6d1516a0925e5857b95d033acb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "facfee9e1060963d861f",
+    "url": "/newsWebsite/static/js/runtime~main.facfee9e.js"
+  },
+  {
+    "revision": "d37d9d51bc42954f425c",
+    "url": "/newsWebsite/static/js/main.d37d9d51.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/newsWebsite/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "d37d9d51bc42954f425c",
+    "url": "/newsWebsite/static/css/main.041d686d.chunk.css"
+  },
+  {
+    "revision": "95953989f8273f5b05b73c6401da600f",
+    "url": "/newsWebsite/index.html"
+  }
+];
